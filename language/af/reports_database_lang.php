5cfeb59180427e77c3c47c9d825df09326223638
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:16:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Die Verslae Databasis bied &#39;n gemeenskaplike stel gereedskap vir die bestuur van databasis-gedrewe verslae.';
+$lang['reports_database_app_name'] = 'verslae Databasis';
