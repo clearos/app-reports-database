c2e7c31771d5545be189f9dae8d1aac8e25238e4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:27:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'The Reports K fana ka sete le tloaelehileng la lithulusi ho laola litlaleho tse polokelongtshedimosetso-khannoang.';
+$lang['reports_database_app_name'] = 'Reports K';
