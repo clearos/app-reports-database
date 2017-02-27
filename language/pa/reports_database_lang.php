d11c50b5d86893610653445900a62c5644de6685
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:25:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'ਰਿਪੋਰਟ ਡਾਟਾਬੇਸ ਡਾਟਾਬੇਸ ਨੂੰ-ਚਲਾਏ ਰਿਪੋਰਟ ਦੇ ਪ੍ਰਬੰਧਨ ਲਈ ਸੰਦ ਦੀ ਇੱਕ ਆਮ ਸੈੱਟ ਦਿੰਦਾ ਹੈ.';
+$lang['reports_database_app_name'] = 'ਰਿਪੋਰਟ ਡਾਟਾਬੇਸ';
