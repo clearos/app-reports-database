927e23c2d203c2e61d5295a2d7fe5f2e15dbbba7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:26:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Baza podatkov Poročila zagotavlja skupen nabor orodij za upravljanje poročil baze podatkov usmerjenih.';
+$lang['reports_database_app_name'] = 'poročila Database';
