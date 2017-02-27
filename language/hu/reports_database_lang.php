e98fb7c1cc7bbcb3b24ed9bbb79a6d23c9ac2e68
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:21:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'A jelentések Database egy sor közös eszközök kezelésére, adatbázis alapú jelentéseket.';
+$lang['reports_database_app_name'] = 'jelentések Database';
