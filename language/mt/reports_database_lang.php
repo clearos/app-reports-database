356066b054e34e7588bd37c99a81ef6f15f8f5da
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:24:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Id-Database Rapporti jipprovdi sett komuni ta &#39;għodod għall-ġestjoni rapporti mmexxija mill-database.';
+$lang['reports_database_app_name'] = 'rapporti Database';
