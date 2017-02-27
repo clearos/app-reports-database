9b09a822d3b5eb8ddcb63ad721b28c143cb7e826
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:18:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'The Malipoti Nawonso achichepere amapereka akonzedwa wamba zipangizo za kayendesedwe malipoti Nawonso achichepere-ceni.';
+$lang['reports_database_app_name'] = 'malipoti Nawonso achichepere';
