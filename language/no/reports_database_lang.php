2c7388e0e088da8b701af340252b995254b0382b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:25:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Rapporter-database gir et felles sett med verktøy for å administrere databasedrevne rapporter.';
+$lang['reports_database_app_name'] = 'rapporter Database';
