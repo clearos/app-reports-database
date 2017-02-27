a212e3c1e507daaf55ef7554759edf7330aa1bbf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:23:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Ny tatitra dia manome iraisana Database hametraka ny fitaovana ho an&#39;ny fitantanana banky angona-noroahina tatitra.';
+$lang['reports_database_app_name'] = 'Reports Database';
