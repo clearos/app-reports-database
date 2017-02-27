25bbc222d21489b9ad1418104fd7fb6d13f47d17
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:28:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Raporlar Veritabanı veritabanına dayalı raporlar yönetmek için araçları ortak bir kümesi sağlar.';
+$lang['reports_database_app_name'] = 'Raporlar Veritabanı';
