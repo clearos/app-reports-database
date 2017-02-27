bc728077da3cc1ad837cb56f942fc0a9c4f14208
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:22:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Есептер Дерекқор дерекқор қамтымды есептерді басқару үшін құралдар жалпы жиынтығы қамтамасыз етеді.';
+$lang['reports_database_app_name'] = 'Есептер Database';
