60e765232bbf3f9e3d8eff660936842056733b4f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:26:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Databáza Reports poskytuje spoločnú sadu nástrojov pre správu zostáv databázy-riadené.';
+$lang['reports_database_app_name'] = 'správy Database';
