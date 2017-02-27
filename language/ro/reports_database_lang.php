8a313c286b914dc6abd29f37a85fd2d701cd2310
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:26:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Rapoartele de Baza de date oferă un set comun de instrumente pentru gestionarea rapoartelor de baze de date bazate.';
+$lang['reports_database_app_name'] = 'rapoarte baze de date';
