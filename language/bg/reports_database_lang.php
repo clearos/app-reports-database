3b1d45291cbbccad526af68a857585ffbe640a4b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:17:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Базата данни за доклади предвижда общ набор от инструменти за управление на бази данни задвижване доклади.';
+$lang['reports_database_app_name'] = 'Доклади Database';
