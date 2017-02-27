5d7a052333c3316c5ec033a09a20d22cf6479ad0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:27:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'La base de datos de informes proporciona un conjunto común de herramientas para la gestión de informes de bases de datos.';
+$lang['reports_database_app_name'] = 'Base de datos de informes';
