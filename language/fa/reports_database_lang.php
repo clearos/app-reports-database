a4d119a4c498adf9320c8d07f50ecdc2378e8498
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:25:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'پایگاه داده گزارش یک مجموعه مشترک از ابزار برای مدیریت گزارش پایگاه داده محور را فراهم می کند.';
+$lang['reports_database_app_name'] = 'پایگاه گزارش';
