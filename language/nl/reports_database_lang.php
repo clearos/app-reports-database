c98aef05ba39ec7c09ec9813d6b829a309ab93b9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:25:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'De Rapporten Database biedt een gemeenschappelijke set van tools voor het beheren van database-driven rapporten.';
+$lang['reports_database_app_name'] = 'rapporten Database';
