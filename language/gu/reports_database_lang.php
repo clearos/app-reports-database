93b2ff9d4ff4c1f5c7037563bba9e19837ef9d43
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:20:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'અહેવાલ ડેટાબેઝ ડેટાબેઝ આધારિત અહેવાલો વ્યવસ્થા કરવા માટે સાધનો સામાન્ય સમૂહ પૂરો પાડે છે.';
+$lang['reports_database_app_name'] = 'અહેવાલ ડેટાબેઝ';
