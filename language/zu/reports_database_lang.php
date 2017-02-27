1209e96e87cc09c52083cdd2080a82e4d7737766
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:30:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'I Database Imibiko inikeza iqoqo amathuluzi for ukulawula imibiko database-enophephela.';
+$lang['reports_database_app_name'] = 'Imibiko Database';
