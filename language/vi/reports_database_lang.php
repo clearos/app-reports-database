f819ed900083ae11055ea8803b1c23fac167ad87
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:29:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Cơ sở dữ liệu báo cáo cung cấp một tập các công cụ để quản lý báo cáo cơ sở dữ liệu-driven.';
+$lang['reports_database_app_name'] = 'báo cáo Cơ sở dữ liệu';
