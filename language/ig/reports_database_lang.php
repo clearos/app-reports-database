548604b40dfbcdf90658f7e8005eebcf0311863b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:21:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'The Akụkọ Database na-enye a nkịtị set ngwaọrụ maka ijikwa nchekwa data-chụpụrụ akụkọ.';
+$lang['reports_database_app_name'] = 'Reports database';
