facae2eac47bb736dc8aec48603dc4e9ddef8413
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:28:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'База даних звітів надає загальний набір інструментів для управління звітами на основі баз даних.';
+$lang['reports_database_app_name'] = 'Звіти бази даних';
