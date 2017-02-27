887940c664586cee81a2b329d57cf79ee92faa56
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:16:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'توفر قاعدة بيانات تقارير مجموعة مشتركة من أدوات لإدارة تقارير قاعدة بيانات يحركها.';
+$lang['reports_database_app_name'] = 'قاعدة بيانات التقارير';
