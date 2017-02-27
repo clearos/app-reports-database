fa0fbb0b4cccd53f45334597af29f9ad5efa53e2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:20:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'The Rahotanni Database samar da wani na kowa sa na kayayyakin aiki, don manajan database-kore rahotannin.';
+$lang['reports_database_app_name'] = 'rahotanni Database';
