fc52596eb6a9e10c07b70eae33352e000ee41891
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:29:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Hisobotlar bazasi ma&#39;lumotlar bazasi-gijgijlash hisobotlarni boshqarish uchun vositalar umumiy majmuini beradi.';
+$lang['reports_database_app_name'] = 'hisobotlar bazasi';
