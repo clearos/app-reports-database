53d777aa28a81dcf66d81b390af1ed0f71c35b25
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:21:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Soláthraíonn an Bunachar Sonraí Tuarascálacha comhshraith uirlisí do thuarascálacha bunachar-tiomáinte a bhainistiú.';
+$lang['reports_database_app_name'] = 'Bunachar Sonraí tuarascálacha';
