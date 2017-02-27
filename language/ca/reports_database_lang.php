9b78f13b1c2d65638d50d0fbfd676bcc539abfaa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:18:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'La base de dades d&#39;informes proporciona un conjunt comú d&#39;eines per a la gestió d&#39;informes de bases de dades.';
+$lang['reports_database_app_name'] = 'Base de dades d&#39;informes';
