8ebc99d05df669cc1df662e95fbc149cabb05797
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:19:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Aruannete andmebaas pakub ühised juhtimise vahendite andmebaasil põhineva aruanded.';
+$lang['reports_database_app_name'] = 'Aruanded Database';
