3ed27955c0b37286aab308f136767e40cb6b8477
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:28:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'ฐานข้อมูลรายงานให้ชุดทั่วไปของเครื่องมือสำหรับการจัดการรายงานฐานข้อมูล';
+$lang['reports_database_app_name'] = 'ฐานข้อมูลรายงาน';
