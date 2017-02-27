9d4f32a5442d7f8936de87af3bdd3b64f9468b50
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:28:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'నివేదికలు డేటాబేస్ డేటాబేస్ నడిచే నివేదికలు నిర్వహణా సాధనాలు ఒక సాధారణ సెట్ అందిస్తుంది.';
+$lang['reports_database_app_name'] = 'నివేదికలు డేటాబేస్';
