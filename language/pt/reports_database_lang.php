b5a1d9167548754caf1fd9f1a9a6fb357e617a56
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:25:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'O Relatórios de banco de dados fornece um conjunto comum de ferramentas para gerenciar relatórios de banco de dados orientado.';
+$lang['reports_database_app_name'] = 'relatórios de banco de dados';
