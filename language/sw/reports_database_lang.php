c580049ef97297419b7166fa448868e67551294d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:27:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Ripoti Database hutoa seti moja ya zana kwa ajili ya kusimamia ripoti database inayotokana.';
+$lang['reports_database_app_name'] = 'ripoti Database';
