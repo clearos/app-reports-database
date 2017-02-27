a701713c6118c1c984e8640356b357078a909407
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:17:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Զեկույցները Database տրամադրում է ընդհանուր շարք գործիքների կառավարման տվյալների բազայի վրա հիմնված հաշվետվություններ.';
+$lang['reports_database_app_name'] = 'Ռեպորտաժ Database';
