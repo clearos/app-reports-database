3c03b9ab70d715e395cedeba242f316483a232e5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:23:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Te Database Pūrongo whakarato te huinga noa o taputapu mō te whakahaere i ngā pūrongo pātengi raraunga-peia.';
+$lang['reports_database_app_name'] = 'pūrongo Pātengi Raraunga';
