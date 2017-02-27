e753bd3627b50fa2055e9476abe1055ab5f71172
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:18:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'အဆိုပါအစီရင်ခံစာများ Database ကိုဒေတာဘေ့စမောင်းနှင်အစီရင်ခံစာများကိုစီမံခန့်ခွဲမှုအတွက် tools တွေကိုတစ်ဘုံအစုံပေးစွမ်းသည်။';
+$lang['reports_database_app_name'] = 'အစီရင်ခံစာများကို Database';
