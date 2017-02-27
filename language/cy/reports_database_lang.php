011d1c5b9acc2b9af9f9363124c60e7eae4d2d0a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:29:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Mae Cronfa Ddata Adroddiadau darparu set gyffredin o offer ar gyfer rheoli adroddiadau a yrrir gan gronfa ddata.';
+$lang['reports_database_app_name'] = 'adroddiadau Cronfa Ddata';
