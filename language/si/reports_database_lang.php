e736996da2f6ee6433a748617e3aefc9130cbd4a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:26:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'එම වාර්තා දත්ත සමුදාය දත්ත සමුදාය මත පදනම් වාර්තා කළමණාකරනය කිරීම සඳහා මෙවලම් පොදු කට්ටලයක් සපයයි.';
+$lang['reports_database_app_name'] = 'වාර්තා දත්ත සමුදාය';
