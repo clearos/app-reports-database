2c84d080be791bb958fc91cd8ad15ee8f8bee026
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:16:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Raportet Baza e të dhënave ofron një grup të përbashkët të mjeteve për menaxhimin e bazës së të dhënave raporte të shtyrë.';
+$lang['reports_database_app_name'] = 'raportet Baza e të dhënave';
