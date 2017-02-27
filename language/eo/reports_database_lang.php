085939b5151cf1e0dfd132db9b7101548bc2b720
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:19:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'La Raportoj Datumbazo disponigas komunan aron de iloj por administri datumbazon tirataj raportoj.';
+$lang['reports_database_app_name'] = 'raportoj Datumbazo';
