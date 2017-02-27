2e9fe69cedf3479c03dc0f174596613b099dc76c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:20:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Rapò baz done a bay yon seri komen nan zouti pou jere rapò baz done-kondwi.';
+$lang['reports_database_app_name'] = 'Rapò baz done';
