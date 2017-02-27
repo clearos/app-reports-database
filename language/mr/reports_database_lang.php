0f9cb3fd23aff40516f6efcbf5b4ff91a327548b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:24:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'अहवाल डाटाबेस डेटाबेस-चेंडू अहवाल व्यवस्थापीत करण्यासाठी साधने एक सामान्य पुरवते.';
+$lang['reports_database_app_name'] = 'अहवाल डाटाबेस';
