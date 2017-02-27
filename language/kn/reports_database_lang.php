ffb13913a7579d7eb1deb2f2922ef6be316422e9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:22:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'ವರದಿಗಳು ಡೇಟಾಬೇಸ್ ಡೇಟಾಬೇಸ್ ಚಾಲಿತ ವರದಿಗಳು ನಿರ್ವಹಿಸುವ ಉಪಕರಣಗಳು ಒಂದು ಸಾಮಾನ್ಯ ಸೆಟ್ ಒದಗಿಸುತ್ತದೆ.';
+$lang['reports_database_app_name'] = 'ವರದಿಗಳು ಡೇಟಾಬೇಸ್';
