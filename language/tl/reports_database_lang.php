1b2d84854977cb8288719e1ed5407bc34c072d47
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:28:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Ang Ulat Database ay nagbibigay ng isang karaniwang hanay ng mga tool para sa pamamahala ng database-driven na mga ulat.';
+$lang['reports_database_app_name'] = 'Reports Database';
