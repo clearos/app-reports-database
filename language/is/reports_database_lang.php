df7614a4632ea9421a89b6fbc770b3d7daec8b90
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:21:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'The Skýrslur Database afla sameiginlega verkfæri til að stjórna gagnagrunn-ekin skýrslur.';
+$lang['reports_database_app_name'] = 'skýrslur Database';
