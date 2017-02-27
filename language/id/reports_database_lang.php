33c673ebad557b7e77522135ad65c2871df691d0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:21:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Laporan Database menyediakan seperangkat alat untuk mengelola laporan database-driven.';
+$lang['reports_database_app_name'] = 'laporan database';
