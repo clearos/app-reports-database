3862050b051200d55ea207b3f82763ccc2526d66
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:24:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Тайлан мэдээллийн сан тулгуурласан тайланг удирдах хэрэгсэл нийтлэг багц олгодог.';
+$lang['reports_database_app_name'] = 'тайлан мэдээллийн сан';
