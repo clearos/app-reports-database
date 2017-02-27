282101d4aa03fbb6fbc9332ba94b5e9ca73d98e4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:18:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Izvješća baze podataka osigurava zajednički set alata za upravljanje baze podataka-driven izvješća.';
+$lang['reports_database_app_name'] = 'Izvješća baze podataka';
