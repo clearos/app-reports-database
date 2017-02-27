a4686a4cab9833c75cfa4fe2ecba7a9e50bd2a2b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:24:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Laporan Pangkalan Data menyediakan satu set biasa alat untuk menguruskan laporan pangkalan data yang didorong oleh.';
+$lang['reports_database_app_name'] = 'laporan Pangkalan Data';
