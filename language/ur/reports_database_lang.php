8fd4e196299f81688ebf16d852622956eab4cb58
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:29:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'رپورٹیں ڈیٹا بیس ڈیٹا بیس پر مبنی رپورٹوں کے انتظام کے لئے آلات کی ایک عام سیٹ فراہم کرتا ہے.';
+$lang['reports_database_app_name'] = 'رپورٹیں ڈیٹا بیس';
