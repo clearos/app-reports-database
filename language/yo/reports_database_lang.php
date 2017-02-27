19417c209e8b701a8fb7d6adb68027a20d7f8cf3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:30:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Awọn Iroyin data pese a wọpọ ṣeto ti irinṣẹ fun ìṣàkóso database-ìṣó iroyin.';
+$lang['reports_database_app_name'] = 'iroyin data';
