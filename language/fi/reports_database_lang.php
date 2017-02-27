a81396a26f65780d79e62408e6e714d335e45886
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:19:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Raportit Database tarjoaa yhteiset työvälineet hallintaan tietokantapohjaista raportteja.';
+$lang['reports_database_app_name'] = 'raportit Database';
