07e0a7bc85072cdd56d034366b2a5f9540928698
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:20:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Η Βάση Δεδομένων Αναφορές παρέχει ένα κοινό σύνολο εργαλείων για τη διαχείριση των αναφορών βάσης δεδομένων με γνώμονα.';
+$lang['reports_database_app_name'] = 'αναφορές Database';
