829fca76be363356f0731bd0ad59f264630dbdc7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:23:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Ataskaitos duomenų bazė suteikia bendrą rinkinį įrankių duomenų bazių ataskaitas valdyti.';
+$lang['reports_database_app_name'] = 'ataskaitos duomenų bazė';
