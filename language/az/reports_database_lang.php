94e4b975e9b27bbd70bf5cd5bd481c0f19ac79dd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:17:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Hesabatlar Database verilənlər bazası idarə hesabat idarə olunması üçün alətlər ümumi təmin edir.';
+$lang['reports_database_app_name'] = 'Hesabatlar Database';
