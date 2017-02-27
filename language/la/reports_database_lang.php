59ab84f3895fc4390f618aacd124f92ce750d5d9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:22:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Renuntiationes database provides instrumenta et administrandi communi statuto database agitatae referuntur.';
+$lang['reports_database_app_name'] = 'Renuntiationes Database';
