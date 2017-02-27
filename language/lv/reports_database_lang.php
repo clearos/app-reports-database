11dddb1cba0ae386ed3ae801a6e4f6544907e8e3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:23:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Ziņojumos Database nodrošina kopēju instrumentu kopumu, lai datubāzi orientētu ziņojumus pārvaldību.';
+$lang['reports_database_app_name'] = 'ziņojumi Database';
