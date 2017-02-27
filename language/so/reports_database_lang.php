2b125edb950d86dbd05c2600090a9d11129c5081
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:27:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Database Wararka waxay bixisaa set caadi ah ee qalabka lagu maareeyo wararka database-driven.';
+$lang['reports_database_app_name'] = 'Wararka Database';
