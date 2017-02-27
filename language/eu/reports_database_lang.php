51fbc1902af9bd56fefcd78dc26e7a49fae166df
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:17:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Txostenak Database datu-basea zuzendutako txostenak kudeatzeko tresna multzo komun bat eskaintzen du.';
+$lang['reports_database_app_name'] = 'Txostenak Database';
