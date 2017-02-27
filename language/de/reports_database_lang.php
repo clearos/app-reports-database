e183259012295164d72bfaff7e20f1821d6914a0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:20:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/reports_database_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['reports_database_app_description'] = 'Die Berichte Datenbank bietet einen gemeinsamen Satz von Tools für Datenbank-gestützte Berichte zu verwalten.';
+$lang['reports_database_app_name'] = 'Berichte Datenbank';
